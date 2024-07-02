85cf522dfc9b1d200907659cf9ddd6a93b1098aa
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-07-01 23:10:59 -0600

    student files updated

--- Project/Student.cpp
+++ Project/Student.cpp
@@ -2,5 +2,8 @@
 
 Student::Student()
 {
+    name = "";
+    code = 0;
+    score = 0.0;
     //ctor
 }
--- Project/Student.h
+++ Project/Student.h
@@ -1,15 +1,42 @@
 #ifndef STUDENT_H
 #define STUDENT_H
+#include <iostream>
 
+using namespace std;
 
 class Student
 {
     public:
+        void setname(string nm)
+        {
+            name=nm;
+        }
+        string getname()
+        {
+            return name;
+        }
+        void setscore(float scr)
+        {
+            score=scr;
+        }
+        float getscore()
+        {
+            return score;
+        }
+        void setcode(int cd)
+        {
+            code=cd;
+        }
+        int getcode()
+        {
+            return code;
+        }
         Student();
 
-    protected:
-
     private:
+        string name;
+        float score;
+        int code;
 };
 
 #endif // STUDENT_H
